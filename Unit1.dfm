7510b697dabb7f1bece61873f30e65315278d705
NaturalSpanking <cahutap7@inbox.ru>
2022-04-17 09:10:29 +0500

    Сделал простой рендеринг одного символа по примеру.

--- Unit1.dfm
+++ Unit1.dfm
@@ -2,7 +2,7 @@ object Form1: TForm1
   Left = 0
   Top = 0
   Caption = 'Form1'
-  ClientHeight = 411
+  ClientHeight = 428
   ClientWidth = 852
   Color = clBtnFace
   Font.Charset = DEFAULT_CHARSET
@@ -13,4 +13,57 @@ object Form1: TForm1
   OldCreateOrder = False
   PixelsPerInch = 96
   TextHeight = 13
+  object Image1: TImage
+    Left = 24
+    Top = 32
+    Width = 193
+    Height = 273
+  end
+  object Button1: TButton
+    Left = 344
+    Top = 48
+    Width = 75
+    Height = 25
+    Caption = 'Button1'
+    TabOrder = 0
+    OnClick = Button1Click
+  end
+  object Memo1: TMemo
+    Left = 544
+    Top = 0
+    Width = 308
+    Height = 428
+    Align = alRight
+    Lines.Strings = (
+      'Memo1')
+    TabOrder = 1
+  end
+  object Button2: TButton
+    Left = 344
+    Top = 88
+    Width = 75
+    Height = 25
+    Caption = 'Font'
+    TabOrder = 2
+    OnClick = Button2Click
+  end
+  object Button3: TButton
+    Left = 344
+    Top = 128
+    Width = 75
+    Height = 25
+    Caption = 'Button3'
+    TabOrder = 3
+    OnClick = Button3Click
+  end
+  object FontDialog1: TFontDialog
+    Ctl3D = False
+    Font.Charset = DEFAULT_CHARSET
+    Font.Color = clWindowText
+    Font.Height = -11
+    Font.Name = 'Tahoma'
+    Font.Style = []
+    Left = 480
+    Top = 16
+  end
 end
--- Unit1.pas
+++ Unit1.pas
@@ -3,13 +3,31 @@ unit Unit1;
 interface
 
 uses
-  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
-  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, uFreeType;
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants,
+  System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, uFreeType, Vcl.StdCtrls, Vcl.ExtCtrls,
+  Vcl.Imaging.jpeg;
 
 type
   TForm1 = class(TForm)
+    Button1: TButton;
+    Memo1: TMemo;
+    Button2: TButton;
+    FontDialog1: TFontDialog;
+    Image1: TImage;
+    Button3: TButton;
+    procedure Button1Click(Sender: TObject);
+    procedure Button2Click(Sender: TObject);
+    procedure Button3Click(Sender: TObject);
   private
-    { Private declarations }
+    ft_lib: TFTLibrary;
+    face: TFTFace;
+    pFont: PByte;
+    font_size: integer;
+    glyph_index: integer;
+    load_flags: TFTLoadFlags;
+    rendef_flags: TFTRenderMode;
+    procedure AddLog(S: string);
   public
     { Public declarations }
   end;
@@ -20,5 +38,95 @@ var
 implementation
 
 {$R *.dfm}
+
+procedure TForm1.AddLog(S: string);
+begin
+  Memo1.Lines.Add(S);
+end;
+
+procedure TForm1.Button1Click(Sender: TObject);
+var
+  x, i, j: integer;
+  major, minor, patch: integer;
+begin
+  x := FT_Init_FreeType(ft_lib);
+  if x <> 0 then
+  begin
+    AddLog('init fail');
+    exit;
+  end;
+
+  FT_Library_Version(ft_lib, major, minor, patch);
+  Memo1.Lines.Add('FreeType''s version is ' + IntToStr(major) + '.' +
+    IntToStr(minor) + '.' + IntToStr(patch));
+
+  font_size := GetFontData(Form1.Canvas.Handle, 0, 0, nil, font_size);
+  GetMem(pFont, font_size);
+  if GetFontData(Form1.Canvas.Handle, 0, 0, pFont, font_size) = GDI_ERROR then
+  begin
+    FreeMem(pFont);
+    FT_Done_FreeType(ft_lib);
+    exit;
+  end;
+
+  // x := FT_New_Face(ft_lib, PAnsiChar(FontDialog1.Font.GetNamePath), 0, face);
+  x := FT_New_Memory_Face(ft_lib, pFont, font_size, 0, face);
+  if x <> 0 then
+  begin
+    AddLog('Failed to load face');
+  end;
+
+  x := FT_Set_Pixel_Sizes(face, 0, 20);
+  if x <> 0 then
+  begin
+    AddLog('Failed to set pixel size');
+  end;
+
+  glyph_index := FT_Get_Char_Index(face, ord('A'));
+
+  load_flags := [ftlfRender];
+  x := FT_Load_Glyph(face, glyph_index, load_flags);
+  if x <> 0 then
+  begin
+    AddLog('Failed to load glyph');
+  end;
+       for i := 0 to face.Glyph.Bitmap.Rows do
+       for j := 0 to face.Glyph.Bitmap.Width do
+         begin
+         Memo1.Lines.Add(IntToStr(face.Glyph.Bitmap.Buffer[i*face.Glyph.Bitmap.Pitch + j]));
+         end;
+
+
+  // разыменовывание указателя происходит тут
+  // к этому моменту в буфере что-то есть уже
+//  x := FT_Render_Glyph(face.Glyph^, ftrmNormal);
+//  if x <> 0 then
+//  begin
+//    AddLog('Failed to render the glyph');
+//  end;
+
+  FT_Done_Face(face);
+  FT_Done_FreeType(ft_lib);
+end;
+
+procedure TForm1.Button2Click(Sender: TObject);
+begin
+  if FontDialog1.Execute(Application.Handle) then
+  begin
+    font_size := 10;
+    Form1.Canvas.Font := FontDialog1.Font;
+  end;
+end;
+
+procedure TForm1.Button3Click(Sender: TObject);
+begin
+  Image1.Picture.Bitmap.Canvas.Pen.Width := 2;
+  Image1.Picture.Bitmap.Canvas.Pen.Color := clBlack;
+  Image1.Picture.Bitmap.Canvas.LineTo(100, 100);
+  Form1.Canvas.Pen.Width := 2;
+  Form1.Canvas.Pen.Color := clYellow;
+  Form1.Canvas.MoveTo(0, 0);
+  Form1.Canvas.LineTo(100, 100);
+end;
 
 end.
