bc1238f6beb2a98194ac251837f7eab7e6bde610
NaturalSpanking <cahutap7@inbox.ru>
2022-05-10 10:11:50 +0500

    Добавил класс TSymbol

--- FontRasterizer.pas
+++ FontRasterizer.pas
@@ -21,6 +21,36 @@ type
     Buffer: PByte;
   end;
 
+  TSymbol = class
+    char_code: integer;
+    Width: integer;
+    Advance: integer;
+    Ascender: integer;
+    Descender: integer;
+    BearingX: integer;
+    BearingY: integer;
+  private
+    BufferSize: integer;
+    Buffer: PByte;
+  public
+    constructor Create(Code: integer);
+    destructor Destroy; override;
+    procedure Free;
+    procedure Render;
+    procedure Show(var Image: TImage);
+    function Build(var f: TextFile): string;
+
+    procedure AddColAtLeft;
+    procedure AddColAtRight;
+    procedure DelColAtLeft;
+    procedure DelColAtRight;
+
+    procedure MoveUp;
+    procedure MoveDown;
+    procedure MoveLeft;
+    procedure MoveRight;
+  end;
+
   P_FR_Font = ^T_FR_Font;
 
   T_FR_Font = record
@@ -38,10 +68,9 @@ var
 function FR_CreateFont: P_FR_Font;
 procedure FR_SetFont(const Font: TFont);
 procedure FR_FreeFont;
+
 procedure FR_ShowSymbol(symbol: PSymb; var Image: TImage);
-
 procedure FR_Render(symb: PSymb);
-
 function FR_CreateSymb(Code: integer): PSymb;
 procedure FR_FreeSymb(symb: PSymb);
 
@@ -442,6 +471,332 @@ begin
       k := 0;
   end;
   fr_face.glyph.Bitmap.Done;
+end;
+
+{ TSymbol }
+
+constructor TSymbol.Create(Code: integer);
+begin
+  char_code := Code;
+  Width := 0;
+  Advance := 0;
+  Ascender := 0;
+  Descender := 0;
+  BearingX := 0;
+  BearingY := 0;
+  BufferSize := 0;
+  Buffer := nil;
+end;
+
+destructor TSymbol.Destroy;
+begin
+  if Buffer <> nil then
+    FreeMemory(Buffer);
+  inherited;
+end;
+
+procedure TSymbol.Free;
+begin
+  if self <> nil then
+    Destroy;
+end;
+
+procedure TSymbol.AddColAtLeft;
+var
+  p: PByte;
+begin
+  p := AllocMem(BufferSize + font_data.bpc);
+  CopyMemory(p + font_data.bpc, Buffer, BufferSize);
+  FreeMemory(Buffer);
+  Buffer := p;
+  inc(Width);
+  inc(BufferSize, font_data.bpc);
+  dec(BearingX);
+end;
+
+procedure TSymbol.AddColAtRight;
+var
+  p: PByte;
+begin
+  p := AllocMem(BufferSize + font_data.bpc);
+  CopyMemory(p, Buffer, BufferSize);
+  FreeMemory(Buffer);
+  Buffer := p;
+  inc(Width);
+  inc(BufferSize, font_data.bpc);
+end;
+
+function TSymbol.Build(var f: TextFile): string;
+var
+  UD: PUniData;
+  i: integer;
+begin
+  if self = nil then
+    exit;
+  UD := UniNamer.Data[self.char_code];
+  writeln(f, '/* ' + 'U+' + UD.U_plus + ' - ' + UD.Name + ' */');
+  write(f, 'static const unsigned char _U_' + UD.U_plus + '[] = {');
+  Result := '_U_' + UD.U_plus;
+  if self.Buffer = nil then
+  begin
+    writeln(f, '/* NO DATA */};');
+    exit;
+  end;
+  write(f, '0x', IntToHex(byte(self.Width)), ', ');
+  for i := 0 to self.BufferSize - 1 do
+    write(f, '0x', IntToHex(self.Buffer[i]), ', ');
+  writeln(f, '};');
+end;
+
+procedure TSymbol.DelColAtLeft;
+var
+  p: PByte;
+begin
+  p := AllocMem(BufferSize - font_data.bpc);
+  CopyMemory(p, Buffer + font_data.bpc, BufferSize - font_data.bpc);
+  FreeMemory(Buffer);
+  Buffer := p;
+  dec(Width);
+  dec(BufferSize, font_data.bpc);
+  inc(BearingX);
+end;
+
+procedure TSymbol.DelColAtRight;
+var
+  p: PByte;
+begin
+  p := AllocMem(BufferSize - font_data.bpc);
+  CopyMemory(p, Buffer, BufferSize - font_data.bpc);
+  FreeMemory(Buffer);
+  Buffer := p;
+  dec(Width);
+  dec(BufferSize, font_data.bpc);
+end;
+
+procedure TSymbol.MoveDown;
+var
+  i: integer;
+  b: integer;
+  Y: integer;
+begin
+  Y := 0;
+  for i := 1 to 8 - (font_data.bpc * 8 - font_data.height) do
+  begin
+    Y := Y shl 1;
+    inc(Y);
+  end;
+  for i := BufferSize - 1 downto 0 do
+  begin
+    if ((Buffer[i - 1] and 128) <> 0) and
+      ((i - 1) mod font_data.bpc <> font_data.bpc - 1) then
+      b := 1
+    else
+      b := 0;
+    if i mod font_data.bpc = font_data.bpc - 1 then
+      Buffer[i] := ((Buffer[i] shl 1) or b) and Y
+    else
+      Buffer[i] := (Buffer[i] shl 1) or b;
+  end;
+end;
+
+procedure TSymbol.MoveLeft;
+var
+  i: integer;
+begin
+  for i := 0 to BufferSize - font_data.bpc do
+  begin
+    Buffer[i] := Buffer[i + font_data.bpc];
+  end;
+  FillChar(Buffer[BufferSize - font_data.bpc], font_data.bpc, 0);
+end;
+
+procedure TSymbol.MoveRight;
+var
+  i: integer;
+begin
+  for i := BufferSize - 1 downto font_data.bpc do
+  begin
+    Buffer[i] := Buffer[i - font_data.bpc];
+  end;
+  FillChar(Buffer[0], font_data.bpc, 0);
+end;
+
+procedure TSymbol.MoveUp;
+var
+  i: integer;
+  b: integer;
+begin
+  for i := 0 to BufferSize - 1 do
+  begin
+    if ((Buffer[i + 1] and 1) <> 0) and ((i + 1) mod font_data.bpc <> 0) then
+      b := 128
+    else
+      b := 0;
+    Buffer[i] := (Buffer[i] shr 1) or b;
+  end;
+end;
+
+procedure TSymbol.Render;
+var
+  P_x, P_y, i, j, k: integer;
+  byte_idx: integer;
+  glyph_index: integer;
+begin
+  if self.Buffer <> nil then
+    FreeMemory(self.Buffer);
+
+  glyph_index := fr_face.GetCharIndex(self.char_code);
+  fr_face.LoadGlyph(glyph_index, [ftlfMonochrome, ftlfTargetMono, ftlfRender]);
+  self.BearingX := fr_face.glyph.Metrics.HorzBearingX div 64;
+  self.BearingY := fr_face.glyph.Metrics.HorzBearingY div 64;
+  font_data.height := fr_face.Size.Metrics.height div 64;
+  self.Width := fr_face.glyph.Bitmap.Width; // это правильнее
+  self.Ascender := fr_face.Size.Metrics.Ascender div 64;
+  self.Descender := fr_face.Size.Metrics.Descender div 64;
+  self.Advance := fr_face.glyph.Metrics.HorzAdvance div 64;
+  if self.char_code = 32 then
+    self.Width := self.Advance;
+
+  if self.Width > font_data.max_w then
+    font_data.max_w := self.Width;
+  if self.Width < font_data.min_w then
+    font_data.min_w := self.Width;
+  self.BufferSize := font_data.bpc * self.Width;
+  self.Buffer := AllocMem(self.BufferSize); // не надо очищать
+  k := 0;
+  for i := 0 to (abs(fr_face.glyph.Bitmap.Rows) *
+    (fr_face.glyph.Bitmap.Pitch)) - 1 do
+  begin
+    for j := 0 to 7 do
+      if fr_face.glyph.Bitmap.Buffer[i] shl j and 128 > 0 then
+      begin
+        P_x := k * 8 + j; // это правильнее
+        P_y := (fr_face.Size.Metrics.Ascender -
+          fr_face.glyph.Metrics.HorzBearingY) div 64 +
+          i div fr_face.glyph.Bitmap.Pitch;
+        byte_idx := (P_y div 8) + font_data.bpc * P_x;
+        // symb.Buffer[byte_idx] := symb.Buffer[byte_idx] + 1 shl (7 - P_y mod 8); так было изначально, символы перевернуты получаются
+        self.Buffer[byte_idx] := self.Buffer[byte_idx] +
+          128 shr (7 - P_y mod 8);
+        // так есть совместимость с GLCD font creator
+      end;
+    inc(k);
+    if k = fr_face.glyph.Bitmap.Pitch then
+      k := 0;
+  end;
+  fr_face.glyph.Bitmap.Done;
+end;
+
+procedure TSymbol.Show(var Image: TImage);
+var
+  i, j: integer;
+  X, Y: integer;
+  grid_size: integer;
+  origin: TPoint;
+  bbox: TRect;
+  R: TRect;
+begin
+  // очистка канвы
+  Image.Picture.Bitmap.Width := Image.Width;
+  Image.Picture.Bitmap.height := Image.height;
+  Image.Canvas.Brush.Color := clGray;
+  Image.Canvas.FillRect(Rect(0, 0, Image.Width, Image.height));
+  if (self = nil) or (self.Buffer = nil) then
+  begin
+    Image.Canvas.Font.Name := 'Times New Roman';
+    Image.Canvas.Font.Size := 14;
+    SetTextAlign(Image.Canvas.Handle, TA_CENTER);
+    Image.Canvas.TextOut(Image.Width div 2, Image.height div 2, 'NO DATA');
+    exit;
+  end;
+  // вычисление размера сетки
+  grid_size := round(Image.height * 7 / 10 / font_data.height);
+  i := round(Image.Width * 5 / 10 / self.Width);
+  if i < grid_size then
+    grid_size := i;
+  // вычисление координат
+  origin.X := round(Image.Width / 8);
+  origin.Y := round(Image.height * 9 / 12);
+  bbox.Left := origin.X;
+  bbox.Right := origin.X + self.Advance * grid_size;
+  bbox.Top := origin.Y - self.Ascender * grid_size;
+  bbox.Bottom := origin.Y - self.Descender * grid_size;
+  // заливка коробки белым
+  Image.Canvas.Brush.Color := clWhite;
+  for X := self.BearingX to self.Width - 1 + self.BearingX do
+    for Y := 0 to font_data.height - 1 do
+    begin
+      R := Rect(bbox.Left + X * grid_size, bbox.Top + Y * grid_size,
+        bbox.Left + X * grid_size + grid_size, bbox.Top + Y * grid_size +
+        grid_size);
+      Image.Canvas.FillRect(R);
+    end;
+  // отрисовка символа
+  Image.Canvas.Brush.Color := clBlack;
+  for i := 0 to self.BufferSize - 1 do
+    for j := 0 to 7 do
+    begin
+      X := i div font_data.bpc + self.BearingX;
+      Y := 8 * (i mod font_data.bpc) + j;
+      R := Rect(bbox.Left + X * grid_size, bbox.Top + Y * grid_size,
+        bbox.Left + X * grid_size + grid_size, bbox.Top + Y * grid_size +
+        grid_size);
+      // if (symbol.Buffer[i] shl j) and 128 > 0 then
+      if (self.Buffer[i] shr j) and 1 > 0 then // совместимость с GLCD
+        Image.Canvas.FillRect(R);
+    end;
+  // отрисовка сетки
+  with Image.Canvas do
+  begin
+    // горизонтальные черные линии
+    pen.Color := clBlack;
+    j := origin.Y;
+    while j > 0 do // выше базовой
+    begin
+      MoveTo(0, j);
+      LineTo(Image.Width, j);
+      dec(j, grid_size);
+    end;
+    j := origin.Y;
+    while j < Image.height do // ниже базовой
+    begin
+      MoveTo(0, j);
+      LineTo(Image.Width, j);
+      inc(j, grid_size);
+    end;
+    // горизонтальные красные линии
+    pen.Color := clRed;
+    MoveTo(0, origin.Y);
+    LineTo(Image.Width, origin.Y);
+    // базовая линия горизонтальная
+    MoveTo(0, bbox.Top);
+    LineTo(Image.Width, bbox.Top); // Ascender
+    MoveTo(0, bbox.Bottom);
+    LineTo(Image.Width, bbox.Bottom); // Descender
+    // вертикальные черные линии
+    pen.Color := clBlack;
+    j := origin.X;
+    while j > 0 do // левее базовой
+    begin
+      MoveTo(j, 0);
+      LineTo(j, Image.height);
+      dec(j, grid_size);
+    end;
+    j := origin.X;
+    while j < Image.Width do // правее базовой
+    begin
+      MoveTo(j, 0);
+      LineTo(j, Image.height);
+      inc(j, grid_size);
+    end;
+    // вертикальные красные линии
+    pen.Color := clRed;
+    MoveTo(origin.X, 0);
+    LineTo(origin.X, Image.height);
+    // базовая линия вертикальная
+    MoveTo(bbox.Right, 0);
+    LineTo(bbox.Right, Image.height); // Advance
+  end;
 end;
 
 end.
